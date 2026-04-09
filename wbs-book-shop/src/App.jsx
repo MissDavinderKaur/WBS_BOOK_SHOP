a722e0530430916432e65b7886886aa78fdd1bf7
MissDavinderKaur <miss_davinder_kaur@hotmail.co.uk>
2026-04-09 09:06:43 +0100

    Basic setup complete

--- wbs-book-shop/src/App.jsx
+++ wbs-book-shop/src/App.jsx
@@ -1,5 +1,15 @@
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
 const App = () => {
-   return <div className='text-2xl'>Hello there</div>;
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
